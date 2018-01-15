4a44cc5fefe84ce534ff1a0589357c2087e99e9f
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2018-01-15 04:45:04 -0800

    Rename smash script

--- sidechain.sh
+++ sidechain.sh
@@ -2,7 +2,6 @@
 
 [[ "${DEBUG}" == 'true' ]] && set -o xtrace
 set -o errexit
-# set -o nounset
 set -o pipefail
 set -o noclobber
 
@@ -15,8 +14,8 @@ readonly __root="$(cd "$(dirname "${__dir}")" && pwd)"
 readonly __manifest="${__dir}/manifest.json"
 readonly __daemon="${__dir}/daemon.json"
 
-readonly -a DEPENDENCIES_PROGRAMS=('jq postgresql postgresql-contrib libpq-dev build-essential python git curl jq libtool autoconf locales automake locate zip unzip htop nmon iftop')
-readonly -a DEPENDENCIES_NODEJS=('pm2 forever grunt-cli')
+readonly -a DEPENDENCIES_PROGRAMS=('postgresql postgresql-contrib libpq-dev build-essential python git curl jq libtool autoconf locales automake locate zip unzip htop nmon iftop')
+readonly -a DEPENDENCIES_NODEJS=('forever grunt-cli')
 # readonly -a DEPENDENCIES_FILES=()
 # readonly -a DEPENDENCIES_PROCESSES=()
 
@@ -25,9 +24,6 @@ source "${__dir}/bootstrap/app.sh"
 
 main()
 {
-    #check_program_dependencies "${DEPENDENCIES_PROGRAMS[@]}"
-    #check_nodejs_dependencies "${DEPENDENCIES_NODEJS[@]}"
-
     parse_args "$@"
 
     trap cleanup SIGINT SIGTERM SIGKILL
