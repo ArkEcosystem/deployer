aaa6f44876f2680c44bfc48c165fec49fde3da2a
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2018-01-15 06:52:52 -0800

    Update dependency checks

--- lib/dependencies.sh
+++ lib/dependencies.sh
@@ -12,46 +12,56 @@ check_program_dependencies()
 {
     local -a dependencies="${1}"
 
+    TO_INSTALL=""
     for dependency in ${dependencies[@]}; do
-        INSTALLED=$(dpkg -l "$dependency" | fgrep "$dependency" | egrep "^[a-zA-Z]" | awk '{print $2}') || true
+        INSTALLED=$(dpkg -l "$dependency" 2>/dev/null | fgrep "$dependency" | egrep "^[a-zA-Z]" | awk '{print $2}') || true
         if [[ "$INSTALLED" != "$dependency" ]]; then
-            read -p "[${dependency}] is not installed. Do you want to install it? [y/N] :" choice
-
-            if [[ "$choice" =~ ^(yes|y) ]]; then
-                success "Installing ${dependency}..."
-                if [[ "$machine" == "Linux" ]]; then
-                    sudo apt-get install "${dependency}" -y
-                elif [[ "$machine" == "Mac" ]]; then
-                    brew install "${dependency}" -y
-                else
-                    abort 1 'Unsupported platform.'
-                fi
-                success 'Installation OK!'
-            else
-                abort 1 "Please ensure that [${dependency}] is installed and try again."
-            fi
+            TO_INSTALL="$TO_INSTALL$dependency "
         fi
     done
+
+    if [[ ! -z "$TO_INSTALL" ]]; then
+        read -p "Dependencies [ ${TO_INSTALL}] are not installed. Do you want to install them? [y/N]: " choice
+
+        if [[ "$choice" =~ ^(yes|y) ]]; then
+            success "Installing dependencies..."
+            if [[ "$machine" == "Linux" ]]; then
+                sudo apt-get install "${TO_INSTALL}" -y
+            elif [[ "$machine" == "Mac" ]]; then
+                brew install "${TO_INSTALL}" -y
+            else
+                abort 1 'Unsupported platform.'
+            fi
+            success 'Installation OK!'
+        else
+            abort 1 "Please ensure that [ ${TO_INSTALL}] dependencies are installed and try again."
+        fi
+    fi
 }
 
 check_nodejs_dependencies()
 {
     local -a dependencies="${1}"
 
+    TO_INSTALL=""
     for dependency in ${dependencies[@]}; do
         INSTALLED=$(npm list -g "$dependency" | fgrep "$dependency" | awk '{print $2}' | awk -F'@' '{print $1}') || true
         if [[ "$INSTALLED" != "$dependency" ]]; then
-            read -p "[${dependency}] is not installed. Do you want to install it? [y/N] :" choice
-
-            if [[ "$choice" =~ ^(yes|y) ]]; then
-                success "Installing ${dependency}..."
-                sudo npm install -g "${dependency}"
-                success 'Installation OK!'
-            else
-                abort 1 "Please ensure that [${dependency}] is installed and try again."
-            fi
+            TO_INSTALL="$TO_INSTALL$dependency "
         fi
     done
+
+    if [[ ! -z "$TO_INSTALL" ]]; then
+        read -p "[ ${TO_INSTALL}] are not installed. Do you want to install them? [y/N]: " choice
+
+        if [[ "$choice" =~ ^(yes|y) ]]; then
+            success "Installing dependencies..."
+            sudo npm install -g "${TO_INSTALL}"
+            success 'Installation OK!'
+        else
+            abort 1 "Please ensure that [ ${TO_INSTALL}] dependencies are installed and try again."
+        fi
+    fi
 }
 
 check_file_dependencies()
