5236a6a65e2498139df836a99d192ffa702f61f3
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2018-02-19 10:26:15 +0000

    Explorer Reward offset to fix negative total

--- app/app-explorer.sh
+++ app/app-explorer.sh
@@ -17,7 +17,7 @@ app_install_explorer()
     rm -rf "$EXPLORER_PATH"
     git clone https://github.com/ArkEcosystem/ark-explorer.git "$EXPLORER_PATH" && cd "$EXPLORER_PATH"
     npm install
-    echo "{\"title\": \"$CHAIN_NAME Explorer\", \"server\": \"http:\/\/$NODE_IP:4100\/api\", \"alias\": \"Sidechain\", \"activeDelegates\": \"$FORGERS\", \"currencies\": [], \"knownWallets\": {}, \"defaults\": {\"currency\": null}, \"config\": {\"priceChart\": false}}" > "$EXPLORER_PATH/networks/sidechain.json"
+    echo "{\"title\": \"$CHAIN_NAME Explorer\", \"server\": \"http:\/\/$NODE_IP:4100\/api\", \"alias\": \"Sidechain\", \"activeDelegates\": \"$FORGERS\", \"rewardOffset\": 0, \"currencies\": [], \"knownWallets\": {}, \"defaults\": {\"currency\": null}, \"config\": {\"priceChart\": false}}" > "$EXPLORER_PATH/networks/sidechain.json"
     mv "$EXPLORER_PATH/package.json" "$EXPLORER_PATH/package.orig.json"
     jq ".scripts.sidechain = \"npm run dev -- --env.network sidechain --env.host=192.168.33.10 --env.port=4200\"" "$EXPLORER_PATH/package.orig.json" > "$EXPLORER_PATH/package.json"
 
