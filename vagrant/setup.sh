5121ff91c9b9f1363d1f81f396828eb5d6b34a4c
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2018-05-14 10:29:18 +0100

    Update ports

--- vagrant/setup.sh
+++ vagrant/setup.sh
@@ -43,6 +43,7 @@ echo '@reboot sleep 15; env USER=$LOGNAME ~/startup.sh' >> ~/cron.sh
 crontab ~/cron.sh
 rm ~/cron.sh
 echo 'Rebooting Vagrant Machine - check back in a few minutes on the below:'
-echo "  Node API: http://127.0.0.1:14100/api/"
+echo "  P2P API: http://127.0.0.1:14102/api/"
+echo "  Public API: http://127.0.0.1:14103/api/"
 echo "  Explorer: http://127.0.0.1:14200/"
 sudo reboot
