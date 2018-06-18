a270285397ab2f23728bad2f0936e0e2927ac17e
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2018-06-18 09:29:04 +0100

    chore: remove explorer server script

--- app/app-explorer.sh
+++ app/app-explorer.sh
@@ -19,12 +19,11 @@ app_install_explorer()
     git clone https://github.com/ArkEcosystem/ark-explorer.git "$EXPLORER_PATH" && cd "$EXPLORER_PATH"
     npm install
     npm install connect-history-api-fallback express
-    cp "$DEPLOYER_PATH/config/explorer-server.js" "$EXPLORER_PATH/server.js"
     echo "{\"title\": \"$CHAIN_NAME Explorer\", \"server\": \"http:\/\/$NODE_IP:$NODE_PORT\/api\", \"alias\": \"Bridgechain\", \"activeDelegates\": \"$FORGERS\", \"rewardOffset\": 0, \"currencies\": [], \"knownWallets\": {}, \"defaults\": {\"currency\": null}, \"config\": {\"priceChart\": false}}" > "$EXPLORER_PATH/networks/bridgechain.json"
     mv "$EXPLORER_PATH/package.json" "$EXPLORER_PATH/package.orig.json"
     jq ".scripts.bridgechain = \"npm run dev -- --env.network=bridgechain --env.host=$EXPLORER_IP --env.port=$EXPLORER_PORT\"" "$EXPLORER_PATH/package.orig.json" > "$EXPLORER_PATH/package.json"
     HOST="$EXPLORER_IP" PORT="$EXPLORER_PORT" node "$EXPLORER_PATH/build/build.js" --network bridgechain
-    echo "HOST=\"$EXPLORER_IP\" PORT=\"$EXPLORER_PORT\" pm2 start $EXPLORER_PATH/server.js --name explorer" > "$EXPLORER_PATH/start-explorer.sh"
+    echo "EXPLORER_HOST=\"$EXPLORER_IP\" EXPLORER_PORT=\"$EXPLORER_PORT\" pm2 start $EXPLORER_PATH/express-server.js --name explorer" > "$EXPLORER_PATH/start-explorer.sh"
     chmod u+x "$EXPLORER_PATH/start-explorer.sh"
 
     success "Explorer Installed!"
