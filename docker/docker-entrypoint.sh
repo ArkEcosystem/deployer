aaf8f38fed46e80a0e2ae69691d4a62d6217a673
Rod Xavier Bondoc <rod.xavier.bondoc@gmail.com>
2018-10-24 13:54:21 +1100

    Dockerize deployer

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -9,3 +9,5 @@ mkdir logs
 CHAIN_NAME=$(cat "$CONFIG_PATH" | jq -r '.chainName')
 echo N | ./bridgechain.sh start-node --name $CHAIN_NAME &>> logs/node.log
 ./bridgechain.sh start-explorer &>> logs/explorer.log
+
+tail -f /dev/null
