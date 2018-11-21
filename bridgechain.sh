e02484b3846111b8bb6cebdf8a73080b8d6c4150
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2018-11-21 21:29:32 +0000

    chore: remove unused redis

--- bridgechain.sh
+++ bridgechain.sh
@@ -13,7 +13,7 @@ readonly __base="$(basename ${__file} .sh)"
 readonly __root="$(cd "$(dirname "${__dir}")" && pwd)"
 readonly __manifest="${__dir}/manifest.json"
 
-readonly -a DEPENDENCIES_PROGRAMS=('postgresql postgresql-contrib build-essential libcairo2-dev pkg-config libtool autoconf automake python git curl jq libpq-dev redis-server ntp')
+readonly -a DEPENDENCIES_PROGRAMS=('postgresql postgresql-contrib build-essential libcairo2-dev pkg-config libtool autoconf automake python git curl jq libpq-dev ntp')
 readonly -a DEPENDENCIES_NODEJS=('pm2 grunt-cli node-sass lerna')
 
 source "${__dir}/bootstrap/lib.sh"
