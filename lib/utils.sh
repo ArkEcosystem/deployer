1eb840e76d5966292e0e26a7e17c6a9f6c79528a
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2019-06-10 16:05:53 +0100

    fix: getting ip for local machines

--- lib/utils.sh
+++ lib/utils.sh
@@ -2,5 +2,22 @@
 
 get_ip()
 {
-    sudo ifconfig | fgrep "inet " | egrep -v "inet (127|192)\." | egrep -o "inet (addr:)?([0-9]+\.?){4}" | awk '{print $2}' | head -n 1
+    IP=$(sudo ifconfig | fgrep "inet " | egrep -v "inet (addr:)?(127|192)\." | egrep -o "inet (addr:)?([0-9]+\.?){4}" | egrep -o "([0-9]+\.?){4}" | head -n 1)
+
+    if [ ! -z "$IP" ]; then
+        echo "$IP"
+    else
+        get_local_ip
+    fi
+}
+
+get_local_ip()
+{
+    IP=$(sudo ifconfig | fgrep "inet " | egrep -v "inet (addr:)?(127)\." | egrep -o "inet (addr:)?([0-9]+\.?){4}" | egrep -o "([0-9]+\.?){4}" | head -n 1)
+
+    if [ ! -z "$IP" ]; then
+        echo "$IP"
+    else
+        echo "127.0.0.1"
+    fi
 }
