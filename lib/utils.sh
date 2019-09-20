1dc6b6d6666315b18975a44494ddc24232df3e1c
Vini Münich <viniciusmunich@gmail.com>
2019-09-19 22:31:05 -0400

    chore: fix indentation

--- lib/utils.sh
+++ lib/utils.sh
@@ -2,17 +2,17 @@
 
 get_ip()
 {
-if AWS_IP=$(curl -f -s --connect-timeout 5 http://169.254.169.254/2009-04-04/meta-data/public-ipv4); then
-    echo "$AWS_IP"
-else
-    IP=$(sudo ifconfig | fgrep "inet " | egrep -v "inet (addr:)?(127|192)\." | egrep -o "inet (addr:)?([0-9]+\.?){4}" | egrep -o "([0-9]+\.?){4}" | head -n 1)
-
-    if [ ! -z "$IP" ]; then
-        echo "$IP"
+    if AWS_IP=$(curl -f -s --connect-timeout 5 http://169.254.169.254/2009-04-04/meta-data/public-ipv4); then
+        echo "$AWS_IP"
     else
-        get_local_ip
+        IP=$(sudo ifconfig | fgrep "inet " | egrep -v "inet (addr:)?(127|192)\." | egrep -o "inet (addr:)?([0-9]+\.?){4}" | egrep -o "([0-9]+\.?){4}" | head -n 1)
+
+        if [ ! -z "$IP" ]; then
+            echo "$IP"
+        else
+            get_local_ip
+        fi
     fi
-fi
 }
 
 get_local_ip()
