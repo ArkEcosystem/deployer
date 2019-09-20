74552e04de81787d85e72d6875eaddc900d4ac86
Vini Münich <viniciusmunich@gmail.com>
2019-09-19 22:29:15 -0400

    fix: get correct external IP on EC2 instances

--- lib/utils.sh
+++ lib/utils.sh
@@ -2,6 +2,9 @@
 
 get_ip()
 {
+if AWS_IP=$(curl -f -s --connect-timeout 5 http://169.254.169.254/2009-04-04/meta-data/public-ipv4); then
+    echo "$AWS_IP"
+else
     IP=$(sudo ifconfig | fgrep "inet " | egrep -v "inet (addr:)?(127|192)\." | egrep -o "inet (addr:)?([0-9]+\.?){4}" | egrep -o "([0-9]+\.?){4}" | head -n 1)
 
     if [ ! -z "$IP" ]; then
@@ -9,6 +12,7 @@ get_ip()
     else
         get_local_ip
     fi
+fi
 }
 
 get_local_ip()
